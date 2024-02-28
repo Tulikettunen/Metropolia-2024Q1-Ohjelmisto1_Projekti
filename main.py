cc672f4b367ef2b6cc804bb5eff580b20d84ca1d
Tuomo Björk <tuomo.bjork@tbk.fi>
2024-02-28 07:10:38 +0200

    create: game/logic.py
    rename game.py -> logic.py, and add call into main.py.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import game.database as database
+import game.logic as game
 
 
 def main():
     database.create_database_connection()
+    game.start()
 
 
 if __name__ == "__main__":
     main()
+
