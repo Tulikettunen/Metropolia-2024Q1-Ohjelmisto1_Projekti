3818b373b01ba0bbd292e8b901230d626dcf4f52
Tuomo Björk <tuomo.bjork@tbk.fi>
2024-02-29 21:45:58 +0200

    add: python rich (for rich text and formatting)
    Added a new requirement to requirements.txt, and added some styling to menus.

--- game/logic.py
+++ game/logic.py
@@ -1,50 +1,78 @@
 import os
 import time
 import random
+from rich import print
+
 
 def start():
     intro()
     mainmenu()
-    #outro()
+    outro()
 
 
 # Printed INTRO menu
 def menu_intro():
-    print(f"Tervetuloa pelaamaan Pakettipilottia!")
-    print(f"""
-   .+---------+
- .' |       .'|                  .
-+----+----+'  |   .. ............;;.
-|   |    |    |    ..::::::::::::;;;;.
-|  .+----+----+  . . ::::::::::::;;:'
-|.'      |  .'                  :'
-+--------+'
-""")
+    print(f"""Tervetuloa pelaamaan Pakettipilottia!
+[#B0C4DE]
+[#BC8F8F]   .+---------+[/#BC8F8F]
+[#BC8F8F] .' |       .'|[/#BC8F8F]                  .
+[#BC8F8F]+----+----+'  |[/#BC8F8F]   .. ............;;.
+[#BC8F8F]|   |    |    |[/#BC8F8F]    ..::::::::::::;;;;.
+[#BC8F8F]|  .+----+----+[/#BC8F8F]  . . ::::::::::::;;:'
+[#BC8F8F]|.'      |  .'[/#BC8F8F]                  :'
+[#BC8F8F]+--------+'[/#BC8F8F]
+[/#B0C4DE]""")
+
+
+def menu_outro():
+    print(f"""Kiitos kun pelasit![#3CB371]
+  _   _  _   _  _                     _  _         _ 
+ | \ | |(_) (_)| |                   (_)(_)       | |
+ |  \| |  __ _ | | __ ___  _ __ ___   _  _  _ __  | |
+ | . ` | / _` || |/ // _ \| '_ ` _ \ | || || '_ \ | |
+ | |\  || (_| ||   <|  __/| | | | | || || || | | ||_|
+ |_| \_| \__,_||_|\_|\___||_| |_| |_||_||_||_| |_|(_)
+[/#3CB371]""")
 
 
 # Printed MAIN menu
 def menu_main():
-    print(f"// [MENU]")
-    print(f"// Valitse syöttämällä luku!")
-    print(f"-- 1: Ohje")
-    print(f"-- 2: Uusi peli")
-    print(f"-- 3: Pistetaulukko")
-    print(f"-- 4: EXIT")
+    text = """
+[#6A5ACD]//[/#6A5ACD] [italic #FF7F50][ALKUVALIKKO][/italic #FF7F50]
+[#6A5ACD]•[/#6A5ACD] [bold blue]1:[/bold blue] Ohje
+[#6A5ACD]•[/#6A5ACD] [bold blue]2:[/bold blue] Uusi peli
+[#6A5ACD]•[/#6A5ACD] [bold blue]3:[/bold blue] Pistetaulukko
+[#6A5ACD]•[/#6A5ACD] [bold blue]4:[/bold blue] EXIT
+"""
+    print(text)
 
 
 # Printed HELP menu
 def menu_help():
-    print("BlaBla")
-    print("BlaBla")
-    print("BlaBla")
+    print(f"""
+[#6A5ACD]//[/#6A5ACD] [italic #FF7F50][OHJE][/italic #FF7F50]
 
+[#6A5ACD]•[/#6A5ACD] Pelin tavoitteena on toimittaa kaikki pelivuoron alussa
+  valitut paketit niiden kohteeseen, ennen määräajan loppumista.
+[#6A5ACD]•[/#6A5ACD] Peliä voi pelata kerrallaan yksi tai useampi henkilö.
+[#6A5ACD]•[/#6A5ACD] Suoritus pisteytetään pelaajan hiilidioksidipäästöjen mukaan.
+\n[yellow]*[/yellow] Paina [green]ENTER[/green] palataksesi alkuvalikkoon [yellow]*[/yellow]""")
 
-# Strtup intro
+# Startup intro
 def intro():
     screen_clear()
     menu_intro()
     time.sleep(2)
     
+    screen_clear()
+
+
+# Exit outro
+def outro():
+    screen_clear()
+    menu_outro()
+    time.sleep(1.5)
+
     screen_clear()
 
 
@@ -74,10 +102,12 @@ def help():
     screen_clear()
     option = False
     while option == False:
+        screen_feedback(option, "none")
         menu_help()
-        print("")
-        option = input("*Paina ENTER palataksesi alkuvalikkoon*\n\n")
+        option = input()
+        
         screen_clear()
+        screen_feedback(option, "none")
 
 
 def newgame():
@@ -94,11 +124,10 @@ def screen_clear():
     else: # Unix/Linux
         os.system('clear')
 
-def screen_feedback(input, feedback_type):
+
+def screen_feedback(user_input, feedback_type):
     if feedback_type == "error":
-        print(f"[ Virheellinen syöte: \"{input}\" ! ]")
-        print("")
+        print(f"[ Virheellinen syöte: [red]\"{user_input}\"[/red] ! ]")
     elif feedback_type == "none":
         print(f"[ {'~' * 24} ]")
-        print("")
 
