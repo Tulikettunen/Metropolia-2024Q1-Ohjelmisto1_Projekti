63d094bb669d3973c70cbb1cba113505ef970397
Tuomo Björk <tuomo.bjork@tbk.fi>
2024-02-29 12:49:12 +0200

    update: game/logic.py (add intro, screen_clear)
    small 2s intro, and screen_clear function.

--- game/logic.py
+++ game/logic.py
@@ -1,14 +1,29 @@
+import os
+import time
+import random
+
 def start():
+    intro()
     mainmenu()
+    outro()
+
+
+# Printed INTRO menu
+def menu_intro():
+    print(f"Tervetulo pelaamaan Pakettipilottia!")
+    print(f"asd")
+    print(f"asdasd")
+    print(f"asdasdasd")
 
 
 # Printed MAIN menu
 def menu_main():
-    print(f"[MENU]")
-    print(f"1: Ohje")
-    print(f"2: Uusi peli")
-    print(f"3: Pistetaulukko")
-    print(f"4: EXIT")
+    print(f"// [MENU]")
+    print(f"// Valitse syöttämällä luku!")
+    print(f"-- 1: Ohje")
+    print(f"-- 2: Uusi peli")
+    print(f"-- 3: Pistetaulukko")
+    print(f"-- 4: EXIT")
 
 
 # Printed HELP menu
@@ -18,26 +33,39 @@ def menu_help():
     print("BlaBla")
 
 
+# Strtup intro
+def intro():
+    screen_clear()
+    menu_intro()
+    time.sleep(2)
+
+
 # Main menu logic
 def mainmenu():
     option = False
+    screen_clear()
+    
     while option != "4":
         menu_main()
         option = input("== ")
-        
         if option == "1":
             help()
+            screen_clear()
         elif option == "2":
             newgame()
+            screen_clear()
         elif option == "3":
             hiscore()
+            screen_clear()
 
 
 # Help menu logic
 def help():
+    screen_clear()
     option = False
     while option != "":
         menu_help()
+        print("")
         option = input("*Paina ENTER palataksesi alkuvalikkoon*")
 
 
@@ -48,4 +76,10 @@ def newgame():
 def hiscore():
     print("3")
 
+
+def screen_clear():
+    if os.name == 'nt': # Windows
+        os.system('cls')
+    else: # Unix/Linux
+        os.system('clear')
 
