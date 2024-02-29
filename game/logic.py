bdcb047b5403066d495313a361ca84fc89785457
Tuomo Björk <tuomo.bjork@tbk.fi>
2024-02-29 22:50:09 +0200

    consolidate
    Remove repetition and refactor for better consistency.

--- game/logic.py
+++ game/logic.py
@@ -5,13 +5,19 @@ from rich import print
 
 
 def start():
+    screen_clear()
     intro()
+    
+    screen_new()
     mainmenu()
+    screen_clear()    
+
     outro()
+    screen_clear()
 
 
-# Printed INTRO menu
-def menu_intro():
+# Startup sequence
+def intro():
     print(f"""Tervetuloa pelaamaan Pakettipilottia!
 [#B0C4DE]
 [#BC8F8F]   .+---------+[/#BC8F8F]
@@ -22,102 +28,81 @@ def menu_intro():
 [#BC8F8F]|.'      |  .'[/#BC8F8F]                  :'
 [#BC8F8F]+--------+'[/#BC8F8F]
 [/#B0C4DE]""")
+    time.sleep(2)
 
 
-def menu_outro():
+# Exit sequence
+def outro():
     print(f"""Kiitos kun pelasit![#3CB371]
-  _   _  _   _  _                     _  _         _ 
- | \ | |(_) (_)| |                   (_)(_)       | |
- |  \| |  __ _ | | __ ___  _ __ ___   _  _  _ __  | |
- | . ` | / _` || |/ // _ \| '_ ` _ \ | || || '_ \ | |
- | |\  || (_| ||   <|  __/| | | | | || || || | | ||_|
- |_| \_| \__,_||_|\_|\___||_| |_| |_||_||_||_| |_|(_)
+ _   _  _   _  _                     _  _         _ 
+| \ | |(_) (_)| |                   (_)(_)       | |
+|  \| |  __ _ | | __ ___  _ __ ___   _  _  _ __  | |
+| . ` | / _` || |/ // _ \| '_ ` _ \ | || || '_ \ | |
+| |\  || (_| ||   <|  __/| | | | | || || || | | ||_|
+|_| \_| \__,_||_|\_|\___||_| |_| |_||_||_||_| |_|(_)
 [/#3CB371]""")
+    time.sleep(1.5)
 
 
-# Printed MAIN menu
-def menu_main():
-    print(f"""
+# Primary game menu
+def mainmenu():
+    option = False
+
+    while option != "4":
+        print(f"""
 [#6A5ACD]//[/#6A5ACD] [italic #FF7F50][ALKUVALIKKO][/italic #FF7F50]
 [#6A5ACD]•[/#6A5ACD] [bold blue]1:[/bold blue] Ohje
 [#6A5ACD]•[/#6A5ACD] [bold blue]2:[/bold blue] Uusi peli
 [#6A5ACD]•[/#6A5ACD] [bold blue]3:[/bold blue] Pistetaulukko
 [#6A5ACD]•[/#6A5ACD] [bold blue]4:[/bold blue] EXIT
 """)
+        option = input(">> ")
+        if option == "1":
+            screen_new()
+            help()
+            screen_new()
+        elif option == "2":
+            screen_new()
+            newgame()
+            screen_new()
+        elif option == "3":
+            screen_new()
+            hiscore()
+            screen_new()
+        else: # If the user input other than predefined, throw error in feedback box.
+            screen_clear()
+            screen_feedback(option, "error")
 
 
-# Printed HELP menu
-def menu_help():
-    print(f"""
+# TBD
+def newgame():
+    option = False
+    # TBD
+
+
+# Help menu screen
+def help():
+    option = False
+    while option == False:
+        print(f"""
 [#6A5ACD]//[/#6A5ACD] [italic #FF7F50][OHJE][/italic #FF7F50]
-
 [#6A5ACD]•[/#6A5ACD] Pelin tavoitteena on toimittaa kaikki pelivuoron alussa
   valitut paketit niiden kohteeseen, ennen määräajan loppumista.
 [#6A5ACD]•[/#6A5ACD] Peliä voi pelata kerrallaan yksi tai useampi henkilö.
 [#6A5ACD]•[/#6A5ACD] Suoritus pisteytetään pelaajan hiilidioksidipäästöjen mukaan.
 
-[yellow]*[/yellow] Paina [green]ENTER[/green] palataksesi alkuvalikkoon [yellow]*[/yellow]""")
-
-# Startup intro
-def intro():
-    screen_clear()
-    menu_intro()
-    time.sleep(2)
-    
-    screen_clear()
-
-
-# Exit outro
-def outro():
-    screen_clear()
-    menu_outro()
-    time.sleep(1.5)
-
-    screen_clear()
-
-
-# Main menu logic
-def mainmenu():
-    option = False
-    screen_feedback(option, "none")
-
-    while option != "4":
-        menu_main()
-        option = input("== ")
-        if option == "1":
-            screen_clear()
-            help()
-        elif option == "2":
-            screen_clear()
-            newgame()
-        elif option == "3":
-            hiscore()
-        else:
-            screen_clear()
-            screen_feedback(option, "error")
-
-
-# Help menu logic
-def help():
-    screen_clear()
-    option = False
-    while option == False:
-        screen_feedback(option, "none")
-        menu_help()
+[yellow]*[/yellow] Paina [green]ENTER[/green] palataksesi alkuvalikkoon [yellow]*[/yellow]
+""")
         option = input()
-        
-        screen_clear()
-        screen_feedback(option, "none")
 
 
-def newgame():
-    print("2")
-
-
+# Hiscore menu logic
 def hiscore():
-    print("3")
+    option = False
+    # TBD
 
 
+# Clear terminal function
 def screen_clear():
     if os.name == 'nt': # Windows
         os.system('cls')
@@ -125,9 +110,17 @@ def screen_clear():
         os.system('clear')
 
 
+# Print feedback box at top of the CLI.
 def screen_feedback(user_input, feedback_type):
     if feedback_type == "error":
         print(f"[ Virheellinen syöte: [red]\"{user_input}\"[/red] ! ]")
     elif feedback_type == "none":
         print(f"[ {'~' * 24} ]")
+
+
+# Clears screen and sets EMPTY feedback box.
+def screen_new():
+    screen_clear()
+    option = False
+    screen_feedback(option, "none")
 
