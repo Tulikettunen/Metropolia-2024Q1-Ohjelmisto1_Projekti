86c36510fdbaf3dd8399b5b9aff83c5f58249649
Noel Kokkonen <viacly1@gmail.com>
2024-03-01 12:01:38 +0200

    Add error handling to create_database_connection()
    This commit will add missing error handling to the create_database_connection -function.

--- game/database.py
+++ game/database.py
@@ -1,6 +1,7 @@
 import os
 import mysql.connector
 from mysql.connector.connection import MySQLConnectionAbstract
+from mysql.connector import Error as DB_error
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -8,21 +9,27 @@ from pathlib import Path
 connection: MySQLConnectionAbstract
 
 
-def create_database_connection():
-    """Establishes connection to the database using environment variables."""
+def create_database_connection() -> DB_error or None:
+    """
+    Establishes connection to the database using environment variables.
+
+    :returns: Error in case of an error and None if no error occur.
+    """
 
     dotenv_path = Path('./config/.env')
     load_dotenv(dotenv_path=dotenv_path)
 
-    global connection
-    connection = mysql.connector.connect(
-        host=os.getenv('HOST'),
-        database=os.getenv('DATABASE'),
-        port=os.getenv('PORT'),
-        username=os.getenv('USER'),
-        password=os.getenv('PASSWORD'),
-        autocommit=bool(os.getenv('AUTOCOMMIT'))
-    )
+    try:
+        global connection
+        connection = mysql.connector.connect(
+            host=os.getenv('HOST'),
+            database=os.getenv('DATABASE'),
+            port=os.getenv('PORT'),
+            username=os.getenv('USER'),
+            password=os.getenv('PASSWORD'),
+            autocommit=bool(os.getenv('AUTOCOMMIT'))
+        )
 
-    if connection:
-        print("Database connection created.")
+        return None
+    except DB_error as error:
+        return error
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import game.database as database
 
 
 def main():
-    database.create_database_connection()
+    err = database.create_database_connection()
+    if err:
+        print(f"Error occurred while creating new database connection: {err}")
+    else:
+        print("Database connection established.")
+        # start the app
 
 
 if __name__ == "__main__":
