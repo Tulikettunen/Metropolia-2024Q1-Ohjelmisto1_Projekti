1ca6eb195cc1e9d4f5f9ca73a16bc5bc905adf20
Noel Kokkonen <viacly1@gmail.com>
2024-03-01 20:08:06 +0200

    Add player highscore insert functions
    This commit will introduce new functions called 'insert_new_player_scores_in_to_db()' and 'fetch_last_game_id_from_db()' these functions can be used to store new player scores to database.
    Also in this commit 'raises'-tags are removed from the docstrings
    because raise is not used.

--- game/database.py
+++ game/database.py
@@ -8,12 +8,11 @@ from pathlib import Path
 connection: MySQLConnectionAbstract
 
 
-def create_database_connection() -> DB_error or None:
+def create_database_connection() -> None | DB_error:
     """
     Establishes connection to the database using environment variables.
 
-    :returns: Error in case of an error and None if no error occur.
-    :raises mysql.connector.Error: If the database connection creation fails
+    :returns: None if connection is established successfully and mysql.connector.Error if error occur
     """
 
     dotenv_path = Path('./config/.env')
@@ -35,13 +34,12 @@ def create_database_connection() -> DB_error or None:
         return error
 
 
-def fetch_10_random_airports_from_db() -> list or DB_error:
+def fetch_10_random_airports_from_db() -> list | DB_error:
     """
     Fetches 10 random airports from database.
 
-    :returns: List of airports as tuples
-    :rtype: [(string, string, float, float)]
-    :raises mysql.connector.Error: If the database fetch fails
+    :returns: List of airports as tuples or mysql.connector.Error if the database fetch fails
+    :rtype: [(string, string, float, float)] or DB_error
     """
 
     sql = ("SELECT airport.name, country.name, airport.latitude_deg, airport.longitude_deg "
@@ -62,13 +60,12 @@ def fetch_10_random_airports_from_db() -> list or DB_error:
         return error
 
 
-def fetch_10_random_parcels_from_db() -> list or DB_error:
+def fetch_10_random_parcels_from_db() -> list | DB_error:
     """
     Fetches 10 random parcels from database.
 
-    :returns: List of parcels as tuple
-    :rtype: [(string, int, int, string)]
-    :raises mysql.connector.Error: If the database fetch fails
+    :returns: List of parcels as tuple or mysql.connector.Error if the database fetch fails
+    :rtype: [(string, int, int, string)] or DB_error
     """
 
     sql = "SELECT item, item_co2, item_type, item_info FROM parcel ORDER BY RAND() LIMIT 10"
@@ -81,5 +78,68 @@ def fetch_10_random_parcels_from_db() -> list or DB_error:
 
         return result
 
+    except DB_error as error:
+        return error
+
+
+def fetch_last_game_id_from_db() -> int | DB_error:
+    """
+    Fetches the last stored game_id from the database
+
+    :returns: Last stored game_id from database or mysql.connector.Error if the last game_id cannot be fetched from the
+    database
+    :rtype: int or DB_error
+    """
+
+    sql = "SELECT MAX(game_id) FROM highscore"
+
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchone()
+
+        if result[0]:
+            return result[0]    # Get the int value out from the result tuple
+        else:
+            return 0    # This happens on the first running time, because no highscore is stored to database yet
+
+    except DB_error as error:
+        return error
+
+
+def insert_new_player_scores_in_to_db(players: list[dict]) -> None | KeyError | DB_error:
+    """
+    Insert new player scores in to the database
+
+    :param players: List of player dictionaries
+    :type players: list[dict]
+    :returns: None if inserting is successful, KeyError If the given dictionary doesn't contain needed key and
+    mysql.connector.Error If the database insert fails or the last game_id couldn't be fetched
+    :rtype: None or KeyError or DB_error
+    """
+
+    cursor = connection.cursor()
+
+    try:
+        last_game_id = fetch_last_game_id_from_db()
+
+        if type(last_game_id) is not int:
+            # Check if error had occur in the fetch_last_game_id_from_db().
+            # In case of an error the error is returned to the caller
+            return last_game_id
+
+        current_game_id = last_game_id + 1  # If the last game_id fetch was successful then add +1 to it
+        for player in players:
+            sql = (f"INSERT INTO highscore (game_id, player_name, player_highscore) "
+                   f"VALUES ({current_game_id}, '{player['playername']}', {player['score']})")
+
+            cursor.execute(sql)
+
+        connection.commit()  # Commit the transaction. (Makes sure that the just inserted values will be stored permanently)
+
+        return None
+    except KeyError as error:
+        return error
     except DB_error as error:
         return error
