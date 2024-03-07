c4397e18f12c3c67132093029af01939d16e13b2
Tuomo Björk <tuomo.bjork@tbk.fi>
2024-03-07 11:54:42 +0200

    update: game/format.py
    rename game/model/format.py -> game/format.py

--- game/format.py
+++ game/format.py
@@ -1,9 +1,16 @@
 # Player dictionary structure
-player_structure = { "name": player_name, "score": False, "co2": 0, "location": False, "parcel_list_picked": False, "parcel_list_delivered": False }
-# Parcel dictionary structure (not defined. decreed by the database itself)
+def player_structure(player_name):
+    return { "name": player_name, "score": False, "co2": 0, "location": False, "parcels_picked": False, "percels_delivered": False }
+
+
+# Parcel dictionary structure (not defined, decreed by the database itself!)
 #parcel_structure = 
+
+
 # Item heft classes
 heft_classes = {1:(0.1,1), 2:(1,3), 3:(3,10)}
+
+
 # Transport method speeds (1: km/h, ... rest are multiples of 1)
 transport_speed1 = 800
 transport_speed2 = 0.75
